a897c08d00701a728192293652a830942d59f3b9
Leonardo2526 <leonardo147wot@gmail.com>
2022-03-12 14:21:12 +0300

    Update Program.cs

--- samples/BenchmarkDotNet.Samples/Program.cs
+++ samples/BenchmarkDotNet.Samples/Program.cs
@@ -1,9 +1,16 @@
 ﻿using BenchmarkDotNet.Running;
+using System;
 
 namespace BenchmarkDotNet.Samples
 {
     public class Program
     {
         public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        //private static void Main(string[] args)
+        //{
+        //    BenchmarkRunner.Run<IntroBasic>();
+
+        //    Console.ReadLine();
+        //}
     }
 }
